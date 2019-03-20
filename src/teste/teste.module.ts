c6e3500b4a99f657f41dd070374f5d674ef0543e
Mario140489 <jmariobck@gmail.com>
2019-03-20 14:50:02 -0300

    Initial commit

---
+++ src/teste/teste.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { EdePipe } from './ede.pipe';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [EdePipe]
+})
+export class TesteModule { }
