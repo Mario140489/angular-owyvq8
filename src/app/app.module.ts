5daf6ae50e7195860bef53896eb9956908bae42a
Mario140489 <jmariobck@gmail.com>
2019-03-22 11:44:16 -0300

    3 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,5 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { BrowserModule} from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
@@ -10,9 +10,10 @@ MatCardModule} from '@angular/material';
 import { cartaoservice } from './service/cartao.service';
 import { HttpModule} from '@angular/http';
 import { NgxCurrencyModule } from "ngx-currency";
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'; 
 @NgModule({
   imports:      [ BrowserModule, FormsModule,MatCheckboxModule,HttpModule,MatCardModule,
-  MatFormFieldModule,MatInputModule,MatIconModule,NgxCurrencyModule],
+  MatFormFieldModule,MatInputModule,MatIconModule,NgxCurrencyModule,BrowserAnimationsModule],
   declarations: [ AppComponent, HelloComponent ],
   bootstrap:    [ AppComponent ],
   providers: [cartaoservice]
