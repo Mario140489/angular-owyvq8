9f34f45b34c3c2f1e16b4b94f8e840c1370b5a0b
Mario140489 <jmariobck@gmail.com>
2019-03-22 14:14:59 -0300

    3 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,10 +10,12 @@ MatCardModule} from '@angular/material';
 import { cartaoservice } from './service/cartao.service';
 import { HttpModule} from '@angular/http';
 import { CurrencyMaskModule } from "ngx-currency-mask";
+import {NgxMaskModule} from 'ngx-mask-2'
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations'; 
 @NgModule({
   imports:      [ BrowserModule, FormsModule,MatCheckboxModule,HttpModule,MatCardModule,
-  MatFormFieldModule,MatInputModule,MatIconModule,CurrencyMaskModule,BrowserAnimationsModule],
+  MatFormFieldModule,MatInputModule,MatIconModule,CurrencyMaskModule,BrowserAnimationsModule,
+     NgxMaskModule.forRoot()],
   declarations: [ AppComponent, HelloComponent ],
   bootstrap:    [ AppComponent ],
   providers: [cartaoservice]
